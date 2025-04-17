360019c142ddf755f797a7e53b9a5f7b56ce2206
Noorpr <11410120210426@stud.cu.edu.eg>
2025-04-17 13:44:56 +0200

    updated the requirments

--- app.py
+++ app.py
@@ -4,18 +4,21 @@ import matplotlib.pyplot as plt
 from query_by_committee import query_active_learning
 from uncertainty_sampling_s1 import uncertainty_sampling_func
 from expected_error_reduction import active_learning_with_eer
+from random_sampling_al import run_random_sampling
 from get_the_dataset import get_breast_cancer, get_iris
 from sklearn.ensemble import RandomForestClassifier
 
 
 st.title("Active Learning Strategies")
-st.subheader("Here is a showdown for the three popular strategies that are used in Active Learning")
+st.subheader("Here is a showdown for the four popular strategies that are used in Active Learning")
 st.markdown("- Uncertainty Sampling")
 st.markdown("- Query By Committee")
 st.markdown("- Expected Error Reduction")
+st.markdown("- Random Sampling")
 
 
 st.write("These strategies are used to select the most informative samples from the pool of unlabeled data")
+st.write("Please note that to show the figures it will take some time so please be patient")
 
 
 # Datasets
@@ -28,7 +31,6 @@ option1 = st.sidebar.selectbox("Dataset:", ["Breast Cancer", "Iris"])
 option2 = st.sidebar.selectbox("which metric do you want:", ['f1', 'recall', 'precision'])
 
 # measures for qbc
-performance_history_qbc = []
 metrics_history_qbc = []
 
 
@@ -40,20 +42,25 @@ metrics_list = []
 results = dict()
 baseModel = RandomForestClassifier(random_state=42)
 
+# measures for rs are inside the if statment
+metrics_history_rs = []
+
 if st.sidebar.button("Apply Option"):
     if option1 == 'Breast Cancer':
-        performance_history_qbc , metrics_history_qbc = query_active_learning(breast_X, breast_y)
+        metrics_history_qbc = query_active_learning(breast_X, breast_y)
         accuracy_list, metrics_list = uncertainty_sampling_func(breast_X, breast_y)
         results = active_learning_with_eer(baseModel, breast_X, breast_y,n_initial=10, n_queries=7)
+        metrics_history_rs = run_random_sampling(breast_X, breast_y, batch_size=1, max_iterations=20)
 
     elif option1 == 'Iris':
-        performance_history_qbc , metrics_history_qbc = query_active_learning(iris_X, iris_y)
+        metrics_history_qbc = query_active_learning(iris_X, iris_y)
         accuracy_list, metrics_list = uncertainty_sampling_func(iris_X, iris_y)
         results = active_learning_with_eer(baseModel, iris_X, iris_y,n_initial=10, n_queries=7)
+        metrics_history_rs = run_random_sampling(iris_X, iris_y, batch_size=1,max_iterations=20)
     
 
-    fig , ax = plt.subplots(nrows=1, ncols=3, figsize=(20,5))
-    ax[0].plot(performance_history_qbc, label='Accuracy', marker='o')
+    fig , ax = plt.subplots(nrows=1, ncols=4, figsize=(20,5))
+    ax[0].plot([m['accuracy'] for m in metrics_history_qbc], label='Accuracy', marker='o')
     ax[0].set_title('Query By Committee')
     ax[0].set_xlabel('Iteration')
     ax[0].set_ylabel('Accuracy')
@@ -75,10 +82,16 @@ if st.sidebar.button("Apply Option"):
     ax[2].grid(True)
     ax[2].legend()
 
-    fig.suptitle("all of three strategies")
+    ax[3].plot([m['accuracy'] for m in metrics_history_rs], label='Accuracy', marker='o')
+    ax[3].set_title("Random Sampling")
+    ax[3].set_xlabel("Iteration")
+    ax[3].set_ylabel("Accuracy")
+    ax[3].grid(True)
+    ax[3].legend()
+
+    fig.suptitle("all of the four strategies")
     
     st.pyplot(fig)
-
 
 
     fig, ax = plt.subplots(figsize=(20, 5))
@@ -86,6 +99,7 @@ if st.sidebar.button("Apply Option"):
     ax.plot([m[option2] for m in metrics_history_qbc], label='Query by Committee', color='green')
     metrics_history_eer = results['metrics_history']
     ax.plot([m[option2] for m in metrics_history_eer], label="Expected Error Reduction", color='orange')
+    ax.plot([m[option2] for m in metrics_history_rs], label="Random Sampling", color='brown')
     ax.set_xticks(range(0, 21, 2))
     ax.set_title(f"{option2} score comparison")
     ax.grid(True)
